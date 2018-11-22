0e96626c0e553ac9a511b635f8895bfaf3124c5b
simon <simon@163.com>
2018-11-22 15:23:54 +0800

    登陆拦截功能完成

---
+++ js/login_intercept.js
@@ -0,0 +1,19 @@
+//将未登录过的用户拦截到登陆页面，只有登陆过后才可以访问页面
+$(function () {
+   $ajax({
+      type: 'get',
+      url: '/employee/checkRootLogin',
+      dataType: 'json',
+      success: function (info) {
+         console.log(info);
+         if (info.success) {
+            close.log('用户已经登陆')
+         };
+
+         if (info.error === 400) {
+
+            location.href = "login.html";
+         }
+      }
+   })
+})
